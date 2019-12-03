72459de376f781806dc468e46e7883cf33657287
Lutz Thielemann <Lutz.Thielemann@gmx.net>
2019-12-03 18:11:05 +0100

    first commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"opensprinkler adapter settings": {
+		"en": "Adapter settings for opensprinkler",
+		"de": "Adaptereinstellungen für opensprinkler",
+		"ru": "Настройки адаптера для opensprinkler",
+		"pt": "Configurações do adaptador para opensprinkler",
+		"nl": "Adapterinstellingen voor opensprinkler",
+		"fr": "Paramètres d'adaptateur pour opensprinkler",
+		"it": "Impostazioni dell'adattatore per opensprinkler",
+		"es": "Ajustes del adaptador para opensprinkler",
+		"pl": "Ustawienia adaptera dla opensprinkler",
+		"zh-cn": "opensprinkler的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
